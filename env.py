4921a2db3ed70099c80c9da6295c8a0478ffa801
cwallacebailey <cwallacebailey@gmail.com>
2022-07-02 11:12:22 +0000

    link to cloudinary, create static and template folders

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://soybsfcfvfpvkf:6f2c9e7d69d9062e9e8a03962d99a49dd196ba9443f113fb5bb194b734fca5e6@ec2-34-242-84-130.eu-west-1.compute.amazonaws.com:5432/d6cqgnjbeo3jng"
 os.environ["SECRET_KEY"] = "s@uthwest!84"
+os.environ["CLOUDINARY_URL"] = "cloudinary://624189548942148:RmTrYOVf13gQzqCqeo-cJuTy12o@dkzungvzx"
