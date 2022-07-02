e2c61bc1fa8ba2a200d0d4f37fcaf64360da4fc6
cwallacebailey <cwallacebailey@gmail.com>
2022-07-02 11:21:35 +0000

    update app name for heroku deployment

--- southwestwalkers/settings.py
+++ southwestwalkers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['south-west-walkers.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['Django-SouthWest-Walkers.herokuapp.com', 'localhost']
 
 
 # Application definition
