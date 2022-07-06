525408fa41e0e5798b62c8d266bd9f10fe998ace
cwallacebailey <cwallacebailey@gmail.com>
2022-07-06 19:07:42 +0000

    fix error in view, missing comma

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import Home, DetailView
 
 urlpatterns = [
-    path('', Home.as_view(), name="home")
-    path('detail/<int:pk>', DetailView.as_view(), name="detail"),
+    path('', Home.as_view(), name="home"),
+    path('detail/<int:pk>', DetailView.as_view(), name='detail'),
 ]
