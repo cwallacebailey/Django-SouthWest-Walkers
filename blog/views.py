b2780b79ea256b219d5dad61d6c0817be8460625
cwallacebailey <cwallacebailey@gmail.com>
2022-07-06 18:19:28 +0000

    link views to urls for home.html

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailedView
+from django.views.generic import ListView
 
 def home(request):
-    return render(request, home.html, {})
+    return render(request, 'home.html', {})
 
 
