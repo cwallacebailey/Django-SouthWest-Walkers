5f976a27c985fd7fc1832bd1eb15ef84e14cbe06
cwallacebailey <cwallacebailey@gmail.com>
2022-07-06 18:46:47 +0000

    create detail view.html to test DetailView class

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 class Home(ListView):
     model = Post
     template_name = 'home.html'
+
+class DetailView(DetailView):
+    model = Post
+    template_name = 'detail_view.html'
