20c07f0852767f996be481eb3e1646939d011c88
cwallacebailey <cwallacebailey@gmail.com>
2022-07-06 18:06:27 +0000

    add home.html to view. Create home.html directory = templates

--- blog/admin.py
+++ blog/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Post
 from django_summernote.admin import SummernoteModelAdmin
 
-# we added summernote in the below
-
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
     
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailedView
+
+def home(request):
+    return render(request, home.html, {})
 
 
