6c3e9203f798398cd72744b3b25bfda1303c7b9a
cwallacebailey <cwallacebailey@gmail.com>
2022-07-06 19:03:12 +0000

    create detail view html page and create link in home.html to detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import Home
+from .views import Home, DetailView
 
 urlpatterns = [
     path('', Home.as_view(), name="home")
+    path('detail/<int:pk>', DetailView.as_view(), name="detail"),
 ]
