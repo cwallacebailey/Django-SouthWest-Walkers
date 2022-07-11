ed20c02628420c7818f6478fbb35a59258f59d26
cwallacebailey <cwallacebailey@gmail.com>
2022-07-11 20:58:27 +0000

    create the form to enter a new post

--- blog/views.py
+++ blog/views.py
@@ -1,16 +1,17 @@
-from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.shortcuts import render, get_object_or_404
+from django.views import generic
 from .models import Post
 
-class Home(ListView):
+class Home(generic.ListView):
     model = Post
     template_name = 'home.html'
+    paginate_by = 8
 
-class DetailView(DetailView):
+class DetailView(generic.DetailView):
     model = Post
     template_name = 'detail_view.html'
 
-class NewPost(CreateView):
+class NewPost(generic.CreateView):
     model = Post
     template_name = 'new_post.html'
     fields = '__all__'
