95a7720b2fdc86e696aa754d1f9271839dec0f85
cwallacebailey <cwallacebailey@gmail.com>
2022-07-12 07:22:22 +0000

    TEMP FIX remove image for new post to test uploading from post form

--- blog/forms.py
+++ blog/forms.py
@@ -2,15 +2,13 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm): 
-    class Meta: 
+    class Meta: # meta class is a class who's instances are classes
         model = Post
-        fields = ('post_title', 'post_author', 'body', 'header_image', 'starred', 'slug')
+        fields = ('post_title', 'post_author', 'body', 'slug')
 
         widgets = {
             'post_title': forms.TextInput(attrs={'class': 'form-control'}),
             'post_author': forms.Select(attrs={'class': 'form-control'}),
             'body': forms.Textarea(attrs={'class': 'form-control'}),
-            'header_image': forms.FileInput(attrs={'required': False,'class': 'form-control','enctype': 'multipart/form-data'}),
-            'starred': forms.Select(attrs={'class': 'form-control'}),
             'slug': forms.TextInput(attrs={'class': 'form-control'}),
         }
