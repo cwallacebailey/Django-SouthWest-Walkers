a43d7d2d4fd7d97d5624a872a63fffb0c0da5245
cwallacebailey <cwallacebailey@gmail.com>
2022-08-08 09:22:29 +0000

    update post model for distance and cairns to climb

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,50 @@ from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 from django.urls import reverse
 
+cairn_choices = (
+    ('Pen y Fan', 'Pen y Fan'),
+    ('Corn Du', 'Corn Du'),
+    ('Fan y Big', 'Fan y Big'),
+    ('Fan Brycheiniog', 'Fan Brycheiniog'),
+    ('Pen Cerrig Calch', 'Pen Cerrig Calch'),
+    ('Picws Du', 'Picws Du'),
+    ('Fan Frynych', 'Fan Frynych'),
+    ('Cribyn', 'Cribyn'),
+    ('Mynydd Llangorse', 'Mynydd Llangorse'),
+    ('Skirrid Fawr', 'Skirrid Fawr'),
+    ('Waun Fach', 'Waun Fach'),
+    ('Twmpa', 'Twmpa'),
+    ('Mynydd Troed', 'Mynydd Troed'),
+    ('The Blorenge', 'The Blorenge'),
+    ('ay Bluff', 'ay Bluff'),
+    ('Pen Y Gadair Fawr', 'Pen Y Gadair Fawr'),
+    ('Sugar Loaf', 'Sugar Loaf'),
+    ('Fan Fawr', 'Fan Fawr'),
+    ('Crug Hywel', 'Crug Hywel'),
+    ('Tor Y Foel', 'Tor Y Foel'),
+)
 
 class Post(models.Model):
+
     post_title = models.CharField(max_length=150, unique=True)
     post_author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
     post_blurb = models.CharField(max_length=100)
     distance = models.FloatField()
+    meters_climbed = models.FloatField(default=0, null=True)
     body = models.TextField()
     header_image = CloudinaryField('image', default='placeholder', null=True, blank=True)
+    image_2 = CloudinaryField('image', default='placeholder', null=True, blank=True)
+    image_3 = CloudinaryField('image', default='placeholder', null=True, blank=True)
+    first_cairn = models.CharField(max_length = 20, choices = cairn_choices , default = '', null=True, blank='true')
+    second_cairn = models.CharField(max_length = 20, choices = cairn_choices , default = '', null=True, blank='true')
+    third_cairn = models.CharField(max_length = 20, choices = cairn_choices , default = '', null=True, blank='true')
     created_date = models.DateTimeField(auto_now_add=True)
     updated_date = models.DateTimeField(auto_now=True) # updates the date instead of creating it anew
     starred = models.ManyToManyField(
         User, blank=True)
+
     
     def total_stars(self):
         return self.starred.count()
