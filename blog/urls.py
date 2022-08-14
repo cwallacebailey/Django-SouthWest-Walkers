d1a3eeb401f6ec76d4d388088893f6cbc6a46f36
cwallacebailey <cwallacebailey@gmail.com>
2022-08-14 14:07:14 +0000

    ensure slideshow function only runs on detail views

--- blog/urls.py
+++ blog/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('', Home.as_view(), name="home"),
     path('detail/<int:pk>', PostDetailView.as_view(), name='detail'),
     path('add_post/', NewPost.as_view(), name='add_post'),
-    path('detail/update/<int:pk>', UpdatePost.as_view(), name='update_post'),
-    path('detail/delete/<int:pk>', DeletePost.as_view(), name='delete_post'),
+    path('update-detail/<int:pk>', UpdatePost.as_view(), name='update_post'),
+    path('delete/<int:pk>', DeletePost.as_view(), name='delete_post'),
     path('star_post/<int:pk>', StarPost.as_view(), name='star_post'),
 
     path('detail/delete/<int:id>', DeleteComment.as_view(), name='delete_comment'),
--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,31 @@
-let next = document.getElementById("next")
-next.addEventListener('click', nextImage);
-let previous = document.getElementById("previous")
-previous.addEventListener('click', backImage);
+if (window.location.pathname.includes('/detail')) {
 
-let imageIndex = 1;
-showImage(imageIndex);
+  let next = document.getElementById("next")
+  next.addEventListener('click', nextImage);
+  let previous = document.getElementById("previous")
+  previous.addEventListener('click', backImage);
 
-function nextImage() {
-    imageIndex += 1;
-    showImage(imageIndex);
-}
+  let imageIndex = 1;
+  showImage(imageIndex);
 
-function backImage() {
-    imageIndex -= 1;
-    showImage(imageIndex);
-}
-
-function showImage(n) {
-  let i;
-  let images = document.getElementsByClassName("journeyImages");
-  if (n > images.length) {imageIndex = 1}
-  if (n < 1) {slideIndex = images.length}
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
+  function nextImage() {
+      imageIndex += 1;
+      showImage(imageIndex);
   }
-  images[imageIndex-1].style.display = "block";
+
+  function backImage() {
+      imageIndex -= 1;
+      showImage(imageIndex);
+  }
+
+  function showImage(n) {
+    let i;
+    let images = document.getElementsByClassName("journeyImages");
+    if (n > images.length) {imageIndex = 1}
+    if (n < 1) {slideIndex = images.length}
+    for (i = 0; i < images.length; i++) {
+      images[i].style.display = "none";
+    }
+    images[imageIndex-1].style.display = "block";
+  }
 }
