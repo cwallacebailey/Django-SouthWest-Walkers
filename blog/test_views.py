f20a240f406a0914d627dcaa701efbe31ce4e6e3
cwallacebailey <cwallacebailey@gmail.com>
2022-08-22 10:45:36 +0000

    ensure test view for add_post works

---
+++ blog/test_views.py
@@ -0,0 +1,8 @@
+from django.test import TestCase
+
+class TestViews(TestCase):
+
+    def test_get_home_page(self):
+        response = self.client.get('add_post/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'new_post.html')
