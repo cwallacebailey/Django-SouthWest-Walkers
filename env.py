5307ca786c09b6da63d6a7b65c3d489fd65395c9
cwallacebailey <cwallacebailey@gmail.com>
2022-08-25 16:14:56 +0000

    fix exposed issue with env.py

--- env.py
+++
@@ -1,6 +0,0 @@
-import os
-
-os.environ["DATABASE_URL"] = "postgres://owumvojxztoygj:1d34f431bec69f9c4500a5b26e3bc3b88abeafe442fa00d3e3c4d7baf5c1cac3@ec2-52-208-164-5.eu-west-1.compute.amazonaws.com:5432/dcs6efs2v9m73e"
-os.environ["SECRET_KEY"] = "s@uthwest!94"
-os.environ["CLOUDINARY_URL"] = "cloudinary://624189548942148:RmTrYOVf13gQzqCqeo-cJuTy12o@dkzungvzx"
-os.environ["PORT"]="8000"
